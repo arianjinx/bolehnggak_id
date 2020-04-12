0dd036e3fabf1196ee92ed1362d4f6fa6dceb519
Arian Pradana <arian.raptor@gmail.com>
2020-04-12 16:14:45 +0700

    [FEATURE] Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#242424`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bolehnggak-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

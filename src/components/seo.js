3538bcce794f63ffa3bf6f21a27e1b1582fee61b
Arian Pradana <arian.raptor@gmail.com>
2020-04-18 07:01:15 +0700

    [FIX] Wrong path

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import ogImage from "../images/bolehnggak_id-og.png"
+import ogImage from "../images/bolehnggak_id-og.jpg"
 
 function SEO({ description, lang, meta, title, isHomePage }) {
   const { site } = useStaticQuery(

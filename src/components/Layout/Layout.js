e3c2bfc3bac5c2e5338a8b0f0e2bf8ad78b9111a
Arian Pradana <arian.raptor@gmail.com>
2020-04-21 01:24:21 +0700

    [FEATURE] Loading state

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,27 +5,36 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import styled from "@emotion/styled"
 import tw from "twin.macro"
 import Footer from "../../containers/Footer/Footer"
 import "./Layout.css"
 
-const Wrapper = styled.div`
-  ${tw`flex flex-col min-h-screen`}
-`
-
-const Main = styled.div`
-  ${tw`flex-grow flex`}
-`
-
 const Layout = ({ children }) => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+
+  const Wrapper = styled.div`
+    ${tw`flex flex-col min-h-screen`}
+  `
+
+  const Main = styled.div`
+    ${tw`flex-grow flex`}
+  `
+
   return (
-    <Wrapper>
-      <Main>{children}</Main>
-      <Footer />
-    </Wrapper>
+    <>
+      {!loading && (
+        <Wrapper>
+          <Main>{children}</Main>
+          <Footer />
+        </Wrapper>
+      )}
+    </>
   )
 }
 
